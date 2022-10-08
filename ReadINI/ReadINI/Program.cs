c249fda83fb71fb34b08550cee65d16e9a3de4fe
ajijo3 <55622247+ajijo3@users.noreply.github.com>
2022-10-08 19:26:47 +0530

    updated

--- ReadINI/ReadINI/Program.cs
+++ ReadINI/ReadINI/Program.cs
@@ -7,8 +7,14 @@ namespace ReadINI
     {
         static void Main(string[] args)
         {
-             string filePath = "Mention path here";
-             List<string> sectionNameList = ReadINI.SectionNames(filePath);
+            string filePath = "Mention path here";
+            List<string> sectionNameList = ReadINI.SectionNames(filePath);
+            foreach (var section in sectionNameList)
+            {
+                    Console.WriteLine("\nSection Name : {0}\n", section);
+                    List<string> keyList = ReadINI.GetKeyList(filePath, section);
+                    List<string>  keys= ReadINI.GetKey(keyList,"Type");
+            }
         }
     }
 }
